1420cc3e7771990dd9502eb3b8e131bb5d2863a5
escowin <edwin@escowinart.com>
2023-01-09 11:59:29 -0600

    18.2.4 | model | comment | schema, association with Pizza, and virtual

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,23 +1,45 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, SchemaType } = require("mongoose");
 
-const PizzaSchema = new Schema({
-  pizzaName: {
-    type: String
+const PizzaSchema = new Schema(
+  {
+    pizzaName: {
+      type: String,
+    },
+    createdBy: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    size: {
+      type: String,
+      default: "Large",
+    },
+    toppings: [],
+    // parent-child association | Pizza tracks comments. the ref property tells Pizza which documents to search to find the right comments.
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
   },
-  createdBy: {
-    type: String
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  size: {
-    type: String,
-    default: 'Large'
-  },
-  toppings: []
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    // id is set to false because this is a virtual that mongoose returns
+    id: false,
+  }
+);
+
+// virtual | adds virtual properties to a document that aren't stored in the db
+// - comment count
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.length;
 });
 
-const Pizza = model('Pizza', PizzaSchema);
+const Pizza = model("Pizza", PizzaSchema);
 
 module.exports = Pizza;
